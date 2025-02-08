0e6501afeb8bea66df857f03fc69343ecfa3ea6a
Lauro Santana <devlauross@gmail.com>
2025-02-08 00:14:33 -0300

    feat: add sql interface

--- driver.go
+++ driver.go
@@ -12,15 +12,27 @@ type Driver struct {
 	dns       string
 	selectt   []byte
 	from      []byte
+	where     []byte
+	insert    []byte
+	values    []byte
 	returning []byte
+	update    []byte
+	set       []byte
+	delete    []byte
 }
 
 func Open(dns string) (driver *Driver) {
 	return &Driver{
 		dns:       dns,
-		selectt:   []byte("SELECT "),
-		from:      []byte(" FROM "),
-		returning: []byte(" RETURNING "),
+		selectt:   []byte("SELECT"),
+		from:      []byte("FROM"),
+		where:     []byte("WHERE"),
+		insert:    []byte("INSERT INTO"),
+		values:    []byte("VALUES"),
+		returning: []byte("RETURNING"),
+		update:    []byte("UPDATE"),
+		set:       []byte("SET"),
+		delete:    []byte("DELETE FROM"),
 	}
 }
 
@@ -57,6 +69,30 @@ func (dr *Driver) From() []byte {
 	return dr.from
 }
 
+func (dr *Driver) Where() []byte {
+	return dr.where
+}
+
+func (dr *Driver) Insert() []byte {
+	return dr.insert
+}
+
+func (dr *Driver) Values() []byte {
+	return dr.values
+}
+
 func (dr *Driver) Returning(b []byte) []byte {
-	return append(dr.returning, append(b, ';')...)
+	return append(dr.returning, b...)
+}
+
+func (dr *Driver) Update() []byte {
+	return dr.update
+}
+
+func (dr *Driver) Set() []byte {
+	return dr.set
+}
+
+func (dr *Driver) Delete() []byte {
+	return dr.delete
 }
